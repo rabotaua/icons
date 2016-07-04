beba581cb947da522613bff1e21faa331f38bead
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-07-04 14:03:51 +0300

    icons

---
+++ README.md
@@ -0,0 +1,17 @@
+[grunt-webfont](https://github.com/sapegin/grunt-webfont)
+
+Requirements
+============
+
+Windows
+-------
+
+Download and install [fontforge](http://fontforge.github.io/en-US/downloads/windows/)
+Download and extract [ttfautohint](http://www.freetype.org/ttfautohint/#download) into `C:\Program Files (x86)\FontForgeBuilds\bin`
+Add `C:\Program Files (x86)\FontForgeBuilds\bin` to %PATH% environment variable
+
+mac OS
+------
+
+	brew install ttfautohint fontforge --with-python
+
---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+﻿module.exports = function (grunt) {
+	grunt.initConfig({
+
+		webfont: {
+			icons: {
+				src: 'svg/*.svg',
+				dest: './dist',
+				options: {
+					font: 'r',
+					syntax: 'bem',
+					engine: 'fontforge',
+					autoHint: true,
+					normalize: true,
+					stylesheet: 'scss',
+					relativeFontPath: 'dist',
+					//relativeFontPath: 'http://css.rabota.com.ua/ra',
+					htmlDemoTemplate: 'template.html',
+					htmlDemoFilename: 'index',
+					templateOptions: {
+						baseClass: 'r',
+						classPrefix: '',
+						mixinPrefix: ''
+					}
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-webfont');
+	grunt.registerTask('default', ['webfont']);
+};
